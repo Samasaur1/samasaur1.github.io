09d2afdbacd234945d0846164ae8c46febf33a45
Sam <30577766+Samasaur1@users.noreply.github.com>
2023-07-19 11:57:17 -0700

    Use my own script to publish drafts

---
+++ .github/scripts/publish-drafts.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -x
+
+CHANGES=NO
+for DRAFT in _drafts/*
+do
+    BASE_FILENAME="${DRAFT#_drafts/}"
+
+    if ! grep '^date: .\+$' "$DRAFT"
+    then
+        continue
+    fi
+    POST_DATE_RAW=$(grep '^date: .\+$' "$DRAFT" | awk '{for (i=2; i<NF; i++) printf $i " "; print $NF}')
+    POST_DATE=$(date -d "$POST_DATE_RAW" "+%s")
+    NOW=$(date "+%s")
+
+    if [ "$NOW" -gt "$POST_DATE" ]
+    then
+        CHANGES=YES
+        POST_DATE_DATE=$(echo "$POST_DATE_RAW" | awk '{print $1}')
+        git mv "$DRAFT" "_posts/${POST_DATE_DATE}-${BASE_FILENAME}"
+    fi
+done
+if [ "$CHANGES" = "$YES" ]
+then
+    git config user.name "GitHub Actions"
+    git config user.email "noreply@github.com"
+    git commit -m "Publish scheduled posts"
+fi
